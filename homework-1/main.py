566f39060c93015529cdfa55b25711be22b1374f
Galaev1 <Amin29_07@mail.ru>
2023-03-28 23:51:29 +0300

    add correction

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,19 +1,20 @@
-from srс.item import Item
+#import srс.item
+from item import Item
 
 if __name__ == '__main__':
-    item1 = pay_rate("Смартфон", 10000, 20)
-    item2 = pay_rate("Ноутбук", 20000, 5)
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
 
     print(item1.calculate_total_price())
-    print(item1.calculate_total_price())
+    print(item2.calculate_total_price())
 
 
     # устанавливаем новый уровень цен
-    pay_rate.pay_rate = 0.8
+    Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
 
-    print(pay_rate.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
