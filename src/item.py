549f653708cb1c893c2f42b98367464c3cb56bac
Galaev1 <Amin29_07@mail.ru>
2023-03-28 23:07:15 +0300

    add HW

--- src/item.py
+++ src/item.py
@@ -1,19 +1,11 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __init__(self,name: str, price: float, quantity: int) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +13,12 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price*self.pay_rate
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        return self.price
+
