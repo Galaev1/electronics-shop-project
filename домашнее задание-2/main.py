fc0996af4ca0457d8eb718e987422b689c0384da
Galaev1 <126718388+Galaev1@users.noreply.github.com>
2023-04-12 20:04:50 +0300

    Update and rename homework-2/main.py to домашнее задание-2/main.py

--- домашнее задание-2/main.py
+++ домашнее задание-2/main.py
@@ -1,5 +1,4 @@
-
-from item import Item
+from src.item import Item
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
@@ -12,10 +11,8 @@ if __name__ == '__main__':
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
-_csv()  # создание объектов из данных файла
-    assert len(Item.all)
-
-    Item.instantiate_from== 5  # в файле 5 записей с данными по товарам
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
 
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
